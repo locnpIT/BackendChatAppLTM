96e6bdf5fb32d052a1553bde594ddb7f0b3ecd72
locnpIT <npl2004qn@gmail.com>
2025-04-23 23:47:12 +0700

    upload

--- src/main/java/com/nguyenphuocloc/ltmchatapp/Security/CustomUserDetails.java
+++ src/main/java/com/nguyenphuocloc/ltmchatapp/Security/CustomUserDetails.java
@@ -50,13 +50,13 @@ public class CustomUserDetails implements UserDetails{
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
+
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
+	
 		return true;
 	}
 
